7545c2d437c91353bc2bb5a0642c402188c463ee
Emmanuel <xtrememanos@windowslive.com>
2024-12-02 12:41:14 +0000

    feat: solved day 2

---
+++ day2/day2.py
@@ -0,0 +1,23 @@
+import pathlib
+path_to_folder: pathlib.Path = pathlib.Path(__file__).resolve().parent
+
+# Duplicate elements are covered by the safe function
+def monotonic(vals: list[int]):
+    return vals == sorted(vals) or vals == sorted(vals, reverse=True)
+
+def safe(vals: list[int]):
+    for i in range(0, len(vals) - 1):
+        a = abs(vals[i] - vals[i + 1])
+        if a > 3 or a < 1:
+            return False
+    return True
+
+c = 0
+with open(pathlib.Path(path_to_folder / "./data.txt"), "r") as file:
+    for line in file:
+        line = line.split()
+        line = [int(x) for x in line]
+        if monotonic(line) and safe(line):
+            c += 1
+        
+print(c)
